ffaa0f3bdfc675843b3c06c118bbffd1232c67f0
Stephane Lafontaine <stephane@medqigong.ca>
2021-05-11 10:00:04 -0400

    Created routes, controller & seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # CRUD CREATE, READ, UPDATE, DELETE
+
+  # verb 'path', to: 'controller#action', as: :prefix
+
+  # # See all restaurants
+  # get '/restaurants', to: 'restaurants#index'
+  
+  # # See one restaurant
+  # get '/restaurants/:id', to: 'restaurants#show'
+
+  # # Add a restaurant >> 2 steps process
+  # # form >> submit >> ???
+  # get '/restaurants/new', to: 'restaurants#new'
+  # post '/restaurants', to: 'restaurants#create'
+
+  # # Modify a restaurant >> 2 steps process
+  # # prefilled form >> submit
+  # get '/restaurants/:id/edit', to: 'restaurants#edit'
+  # patch '/restaurants/:id', to: 'restaurants#update'
+
+  # # Delete a restaurant
+  # delete '/restaurants/:id', to: 'restaurants#destroy'
+
+  resources :restaurants
 end
